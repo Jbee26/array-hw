7d0de4d4ed9477aea5428cefb6388733a0103c1d
Jbee26 <jack_belyea26@milton.edu>
2025-01-15 09:16:08 -0500

    :)

--- src/Book.java
+++ src/Book.java
@@ -7,15 +7,18 @@ public class Book {
     public String author;
     
     public Book(int pRating){
-        title = "Grandmaster of Demonic Cultivation";
-        author = "MO Xiang Tong Xiu";
-        // TODO: set the instance variable to the parameter
+        title = "Stay Hungry";
+        author = "Sebastian Maniscalco";
+        rating = pRating;
 
+
+print();
     }
 
     public void print(){
         System.out.println("title: " + title);
         System.out.println("author: " + author);
+        System.out.println("rating:" + rating);
         //TODO: use System.out to print the values of the
         //      instance varibles so the user can see the review
 
--- src/Library.java
+++ src/Library.java
@@ -1,28 +1,53 @@
 import java.sql.SQLOutput;
+import java.util.Random;
 
 public class Library {
 
-    public String owner = "Mx. Bradford";
+    public String owner = "Jack B";
     public Book[] allBooks;
+    int sum = 0;
     public static void main(String[] args) {
-        //TODO: Make an instance of the main class
+        Library myLibrary = new Library();
     }
+
 
     public Library(){
 
         System.out.println("Welcome to "+ owner + "'s library!!!!!");
         //TODO: Give the array allReviews a size of 10
+        allBooks = new Book[10];
+
+
 
         //TODO: Fill the array allReviews with reviews with random ratings from 0 to 10 inclusive
 
+        for (int b = 0; b < allBooks.length; b++){
+            allBooks[b] = new Book((int)(Math.random() * 11));
+        }
+
+
+
+
         //TODO: print all the ratings in the array allReviews
-        
+
+        for (int p = 0; p < allBooks.length; p++ ){
+            allBooks[p].print();
+        }
+
         //TODO: Call the method averageRating
+        averageRating();
 
     }
 
     public void averageRating(){
         //TODO: calculate the average rating from the array allReviews and print it
 
+
+        for (int s = 0; s < allBooks.length; s++){
+            sum += allBooks[s].rating;
+        }
+
+        double average = (double) sum / allBooks.length;
+        System.out.println(average + "= average ");
     }
 }
